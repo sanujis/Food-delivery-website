fbfbe440e4cb8075d4524c45e4d794f1fbad80e5
sanujis <156413093+sanujis@users.noreply.github.com>
2025-04-07 21:47:57 +0530

    Update StoreContext.jsx

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -1,5 +1,4 @@
 import React, { createContext, useState } from 'react';
-import { food_list } from '../../assets/assets';
 import { useEffect } from 'react';
 
 export const StoreContext = createContext(null);
@@ -8,6 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = "http://localhost:4000";
   const [token, setToken] = useState("");
+  const [food_list, setFoodList] = useState([]);
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({
