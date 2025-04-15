af63103bdb1d88dc69c6babfe66957bf031be282
sanujis <156413093+sanujis@users.noreply.github.com>
2025-04-15 13:15:22 +0530

    changed navbar

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,32 +1,34 @@
-import React,{ useContext } from 'react'
-import './FoodDisplay.css'
-import FoodItem from '../FoodItem/FoodItem'
-import { StoreContext } from '../context/StoreContext'
+import React, { useContext } from 'react';
+import './FoodDisplay.css';
+import FoodItem from '../FoodItem/FoodItem';
+import { StoreContext } from '../context/StoreContext';
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+
+  // Filter food items based on the selected category
+  const filteredFoodList =
+    category === 'All'
+      ? food_list
+      : food_list.filter((item) => item.category === category);
 
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => {
-
-          if (category === 'All'|| item.category === category) {
-            return (
-              <FoodItem
-                key={index}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-              />
-            );    
-          }})}
+        {filteredFoodList.map((item, index) => (
+          <FoodItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
-export default FoodDisplay
+export default FoodDisplay;
